8bf3c23148ca22facb9a277a26a39eca1654248b
Tri M. Nguyen <trimn@myopera.com>
2011-04-22 18:27:54 +0200

    tri goez lazy with commit script and default commit message

--- src/net/trimn/bybuss/AtbBussorakel.java
+++ src/net/trimn/bybuss/AtbBussorakel.java
@@ -6,9 +6,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
+ * This class is used to communicate with AtB.
  * 
  * @author tmn
  */
@@ -16,20 +18,22 @@ public class AtbBussorakel {
 	private String question;
 	private String answer;
 	private URI uri;
-	
-	
+
 	/**
 	 * Create an instance of Busstuc
 	 */
 	public AtbBussorakel() {
 		try {
-			uri = new URI("http", "m.atb.no", "/xmlhttprequest.php?service=routeplannerOracle.getOracleAnswer&question=", null);
+			uri = new URI(
+					"http",
+					"m.atb.no",
+					"/xmlhttprequest.php?service=routeplannerOracle.getOracleAnswer&question=",
+					null);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * Set question
 	 * 
@@ -38,33 +42,30 @@ public class AtbBussorakel {
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	
-	
+
 	/**
-	 * Return question 
+	 * Return question
 	 * 
 	 * @return String
 	 */
 	public String getQuestion() {
 		return this.question;
 	}
-	
 
-	
-	
 	/**
 	 * Get answer fomr Bussorakelet
 	 * 
 	 * @return String
 	 */
 	public void ask() {
-		String content 			= null;
-		URLConnection conn 		= null;
-		Scanner sc 				= null;
-		
-		String tmpUri = this.uri.toString().replace("%3F", "?") + this.getQuestion().replace("%3F", "?");
+		String content = null;
+		URLConnection conn = null;
+		Scanner sc = null;
+
+		String tmpUri = this.uri.toString().replace("%3F", "?")
+				+ this.getQuestion().replace("%3F", "?");
 		tmpUri = tmpUri.replace(" ", "%20");
-		
+
 		try {
 			conn = new URL(tmpUri).openConnection();
 		} catch (MalformedURLException e) {
@@ -72,13 +73,13 @@ public class AtbBussorakel {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
 			sc = new Scanner(conn.getInputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		if (sc != null) {
 			sc.useDelimiter("\\Z");
 			content = sc.next();
@@ -89,7 +90,49 @@ public class AtbBussorakel {
 
 	}
 
+	/**
+	 * Checks if the last word in String is a date and month. This class fixes
+	 * the Formating bug for some special cases.
+	 * 
+	 * @param s
+	 * @return String
+	 */
+	private boolean checkEnding(String s) {
+		ArrayList<String> month = new ArrayList<String>() {
+			private static final long serialVersionUID = 1L;
+			{
+				add("Jan");
+				add("Feb");
+				add("Mar");
+				add("Apr");
+				add("May");
+				add("Jun");
+				add("Jul");
+				add("Aug");
+				add("Sep");
+				add("Oct");
+				add("Now");
+				add("Des");
+			}
+		};
+		boolean result = false;
 
+		for (String s2 : month) {
+			if ((s.substring(s.length() - 3)).equals(s2)) {
+				result = true;
+				break;
+			} else {
+				try {
+					Integer.parseInt(s.substring(s.length() - 2).trim());
+					result = true;
+				} catch (NumberFormatException e) {
+					result = false;
+				}
+			}
+		}
+
+		return result;
+	}
 
 	/**
 	 * Format answer from server.
@@ -101,13 +144,19 @@ public class AtbBussorakel {
 		tmpAnswer = tmpAnswer.replace(" kl. ", " kl ");
 		tmpAnswer = tmpAnswer.replace("  ", " ");
 		String[] answerFormated = tmpAnswer.split("\\. ");
-		
+
 		String finalAnswer = "";
-		
+
 		for (String s : answerFormated) {
-			finalAnswer += s.trim() + ".\n\n";
+
+			if (checkEnding(s)) {
+				finalAnswer += s.trim() + ".";
+			} else {
+				finalAnswer += s.trim() + ".\n\n";
+			}
+
 		}
 		return finalAnswer;
 	}
-	
+
 }
